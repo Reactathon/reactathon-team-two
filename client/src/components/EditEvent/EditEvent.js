639276d0d3c011481ab5a0b0b6a6aea684d73fb5
Daniel Christo <daniel.christo@jonahgroup.com>
2018-01-11 14:27:01 -0500

    Added basic tabbed edit event page
    Added basic tabbed edit event page
    as well as a basic react table
    with example data

---
+++ client/src/components/EditEvent/EditEvent.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+import 'react-tabs/style/react-tabs.css'
+import "react-table/react-table.css";
+
+import {loadProfile} from '../../actions/profileActions'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import ReactTable from "react-table";
+import { makeData } from "./Helper";
+
+class EditEvent extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: makeData()
+        };
+    }
+
+    componentDidMount() {
+        const id = this.props.match.params.id
+        this.props.getProfile(id)
+    }
+
+    render() {
+        const { data } = this.state;
+        return (
+                    <Tabs>
+                    <TabList>
+                        <Tab>Practice Schedule</Tab>
+                        <Tab>Event</Tab>
+                        <Tab>Musician</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <h2>Schedule for Event {this.props.match.params.id}</h2>
+                        <div>
+                            <ReactTable
+                                data={data}
+                                columns={[
+                                    {
+                                        Header: "Name",
+                                        columns: [
+                                            {
+                                                Header: "First Name",
+                                                accessor: "firstName"
+                                            },
+                                            {
+                                                Header: "Last Name",
+                                                id: "lastName",
+                                                accessor: d => d.lastName
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        Header: "Info",
+                                        columns: [
+                                            {
+                                                Header: "Age",
+                                                accessor: "age"
+                                            },
+                                            {
+                                                Header: "Status",
+                                                accessor: "status"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        Header: 'Stats',
+                                        columns: [
+                                            {
+                                                Header: "Visits",
+                                                accessor: "visits"
+                                            }
+                                        ]
+                                    }
+                                ]}
+                                defaultPageSize={10}
+                                className="-striped -highlight"
+                            />
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Location + Schedule List</h2>
+                        <ul>
+                            <li>Location 1 Schedule</li>
+                            <li>Location 2 Schedule</li>
+                        </ul>
+                        <h2>Song List</h2>
+                        <ul>
+                            <li>Song 1</li>
+                            <li>Song 2</li>
+                        </ul>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Musician editing goes here</h2>
+                    </TabPanel>
+                </Tabs>)
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        profile: state.profile
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getProfile: profileId => dispatch(loadProfile(profileId))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditEvent)
