504d1775ca5a9ac145a5b8799fc402c10d12720f
Volodymyr Stepanyuk <vovan-st@users.noreply.github.com>
2018-01-11 16:39:05 -0500

    removed unused button

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -5,7 +5,6 @@ const Landing = () => {
     return (
         <div>
             <h2>Welcome to Jonah's First Reactathon!</h2>
-            <button>Create Event!</button>
             <h2>Events List</h2>
             <Events/>
         </div>
